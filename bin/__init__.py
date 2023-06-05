0c157ca6986d90046884da04eb43a1b42939706d
TRIAEIOU <triaeiou@protonmail.com>
2023-06-05 17:25:39 +0200

    Working

--- bin/__init__.py
+++ bin/__init__.py
@@ -6,11 +6,13 @@ CVER = get_version()
 NVER = "1.0.0"
 
 OSC_DONE = "_overrideshortcut_addon_done_"
+WINDOW_CLOSE = "window close"
 DEFAULTS = os.path.join(os.path.dirname(__file__), 'defaults.json')
 CONFIG_MD = os.path.join(os.path.dirname(__file__), 'config.md')
 INSTR = """Override found shortcuts by adding a key/value pair (see below for identified shortcuts). To unmap a found shortcut add the key with an empty value (`""`) example:
 
 <pre><code>{
+  "window close": "Escape",
   "AnkiQt": {
     "QtClassProxy: action-actionAbout": "Ctrl+Alt+A", // comma between items
     "QtClassProxy: action-actionAdd_ons": "" // empty string will remove binding
@@ -33,12 +35,32 @@ class Finder(aqt.QObject):
     cfg = mw.addonManager.getConfig(__name__)
     windows = {}
     i = {}
+    escape = False
 
     def __init__(self, app: aqt.QApplication):
         with open(DEFAULTS) as fh:
             self.defaults = json.load(fh)
         super().__init__()
         app.focusChanged.connect(self.onfocus)
+
+    def eventFilter(self: object, obj: aqt.QObject, evt: aqt.QEvent):
+        """Used when instance installed as event filter to unmap `Escape` from window close"""
+        evt_type = evt.type()
+        if evt_type == aqt.QEvent.Type.ShortcutOverride and \
+        evt.keyCombination() == aqt.QKeyCombination(aqt.Qt.Key.Key_Escape):
+            self.escape = True
+            evt.accept()
+            return True # block event
+        elif self.escape:
+            if evt_type == aqt.QEvent.Type.Close:
+                self.escape = False
+                evt.ignore()
+                return True # block event
+            elif evt_type == aqt.QEvent.Type.KeyPress and \
+            evt.keyCombination() == aqt.QKeyCombination(aqt.Qt.Key.Key_Escape):
+                return False # don't block event (self.escape still True)
+        self.escape = False
+        return False
   
     @aqt.pyqtSlot(aqt.QWidget, aqt.QWidget)
     def onfocus(self, _old_widget: aqt.QWidget, widget: aqt.QWidget):
@@ -71,12 +93,12 @@ class Finder(aqt.QObject):
                 sequence = get(obj)
 
                 if oname := obj.objectName():
-                    seq_name = f"{class_name}: {oname}"
+                    seq_name = f"{class_name} {oname}"
                 elif sequence:
                     seq_name = f"{class_name} ({sequence})"
                 else:
                     self.i[class_name] = self.i.get(class_name, 0) + 1
-                    seq_name = f"{class_name}-{self.i[class_name]}"
+                    seq_name = f"{class_name} [{self.i[class_name]}]"
 
                 if discovery:
                     self.discovered[win_name][seq_name] = sequence
@@ -86,6 +108,10 @@ class Finder(aqt.QObject):
         # Find all actions and shortcuts, store or override as needed
         find(widget, aqt.QAction, lambda o: o.shortcut().toString(), lambda o, s: o.setShortcut(aqt.QKeySequence(s)))
         find(widget, aqt.QShortcut, lambda o: o.key().toString(), lambda o, s: o.setKey(aqt.QKeySequence(s)))
+        
+        # Unmap escape for window close
+        if (s := self.cfg.get(WINDOW_CLOSE, 'Escape')) != 'Escape':
+            widget.installEventFilter(self)
 
         # Completely overwrite previously found sections with new versions (`|` is non-recursive)
         if discovery:
